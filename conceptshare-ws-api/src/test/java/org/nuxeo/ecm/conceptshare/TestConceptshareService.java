8aebfc5cfb7dcce4f6651eae9ab3ef77c0fca611
Josh Fletcher <jfletcher@nuxeo.com>
2017-10-31 10:23:05 -0700

    Formatting only.

--- conceptshare-ws-api/src/test/java/org/nuxeo/ecm/conceptshare/TestConceptshareService.java
+++ conceptshare-ws-api/src/test/java/org/nuxeo/ecm/conceptshare/TestConceptshareService.java
@@ -25,99 +25,99 @@ import org.nuxeo.runtime.test.runner.LocalDeploy;
 import com.google.inject.Inject;
 
 @RunWith(FeaturesRunner.class)
-@Features({ PlatformFeature.class })
+@Features({PlatformFeature.class})
 @Deploy("org.nuxeo.ecm.conceptshare.ws.api")
 @LocalDeploy("org.nuxeo.ecm.conceptshare.ws.api:conceptshare-service-test-contrib.xml")
 public class TestConceptshareService {
 
-    public static final String DEFAULT_PROJECT = "TestProject";
+  public static final String DEFAULT_PROJECT = "TestProject";
 
-    public static final String TEST_USER_EMAIL = "mhilaire@nuxeo.com";
+  public static final String TEST_USER_EMAIL = "mhilaire@nuxeo.com";
 
-    @Inject
-    protected ConceptshareService conceptshareservice;
+  @Inject
+  protected ConceptshareService conceptshareservice;
 
-    @Test
-    public void testService() {
-        assertNotNull(conceptshareservice);
-    }
+  @Test
+  public void testService() {
+    assertNotNull(conceptshareservice);
+  }
 
-    @Test
-    public void itCanGetProjects() throws Exception {
-        assertTrue(conceptshareservice.getProjects().size()>0);
-    }
+  @Test
+  public void itCanGetProjects() throws Exception {
+    assertTrue(conceptshareservice.getProjects().size() > 0);
+  }
 
-    @Test
-    public void itCanGetDefaultProject() throws Exception {
-        assertEquals(DEFAULT_PROJECT, conceptshareservice.getDefaultProject().getName().getValue());
-    }
+  @Test
+  public void itCanGetDefaultProject() throws Exception {
+    assertEquals(DEFAULT_PROJECT, conceptshareservice.getDefaultProject().getName().getValue());
+  }
 
-    @Test
-    public void itCanCreateReviewAndGetUrl() throws Exception {
-        String reviewTitle = "myReviewUnitTest - " + new Date().getTime();
+  @Test
+  public void itCanCreateReviewAndGetUrl() throws Exception {
+    String reviewTitle = "myReviewUnitTest - " + new Date().getTime();
 
-        Review review = createReview(reviewTitle);
+    Review review = createReview(reviewTitle);
 
-        int userId = conceptshareservice.getUserId(TEST_USER_EMAIL);
+    int userId = conceptshareservice.getUserId(TEST_USER_EMAIL);
 
-        assertFalse(conceptshareservice.getReviewURL(review.getId(), userId).isEmpty());
-    }
+    assertFalse(conceptshareservice.getReviewURL(review.getId(), userId).isEmpty());
+  }
 
-    @Test
-    public void itCanAddMember() throws Exception {
-        String reviewTitle = "myReviewUnitTest - " + new Date().getTime();
-        Review review = createReview(reviewTitle);
+  @Test
+  public void itCanAddMember() throws Exception {
+    String reviewTitle = "myReviewUnitTest - " + new Date().getTime();
+    Review review = createReview(reviewTitle);
 
-        String memberEmail = "nuxeo.demo.dam+administrator@gmail.com";
+    String memberEmail = "nuxeo.demo.dam+administrator@gmail.com";
 
-        ReviewMember member = conceptshareservice.addReviewMember(memberEmail, review.getId());
-        assertEquals(memberEmail, member.getUserName().getValue());
-    }
+    ReviewMember member = conceptshareservice.addReviewMember(memberEmail, review.getId());
+    assertEquals(memberEmail, member.getUserName().getValue());
+  }
 
-    @Test
-    public void itCanAddAssetInReviewAndComplete() throws Exception {
-        long timestamp = new Date().getTime();
-        String reviewTitle = "myReviewUnitTest - " + timestamp;
-        String name = "Unit test - " + timestamp;
-        String filename = name + ".png";
-        Review review = createReview(reviewTitle);
+  @Test
+  public void itCanAddAssetInReviewAndComplete() throws Exception {
+    long timestamp = new Date().getTime();
+    String reviewTitle = "myReviewUnitTest - " + timestamp;
+    String name = "Unit test - " + timestamp;
+    String filename = name + ".png";
+    Review review = createReview(reviewTitle);
 
-        Integer inProgressStatus = conceptshareservice.getReviewStatusId(ConceptshareService.REVIEW_IN_PROGRESS_STATUS);
-        assertEquals(inProgressStatus, review.getStatusId().getValue());
+    Integer inProgressStatus = conceptshareservice.getReviewStatusId(ConceptshareService.REVIEW_IN_PROGRESS_STATUS);
+    assertEquals(inProgressStatus, review.getStatusId().getValue());
 
-        Asset asset = conceptshareservice.addAsset(name, filename, "https://www.nuxeo.com/assets/imgs/logo340x60.png");
+    Asset asset = conceptshareservice.addAsset(name, filename, "https://www.nuxeo.com/assets/imgs/logo340x60.png");
 
-        assertEquals(filename, asset.getFileName().getValue());
+    assertEquals(filename, asset.getFileName().getValue());
 
-        ReviewItem ri = conceptshareservice.addReviewItem(review.getId(), asset.getId());
+    ReviewItem ri = conceptshareservice.addReviewItem(review.getId(), asset.getId());
 
-        assertEquals(asset.getId(), ri.getAssetId());
-        assertEquals(review.getId(), ri.getReviewId());
+    assertEquals(asset.getId(), ri.getAssetId());
+    assertEquals(review.getId(), ri.getReviewId());
 
-        review = conceptshareservice.endReview(review.getId(), reviewTitle, "myDescription", "12345");
-        assertEquals(review.getId(), review.getId());
+    review = conceptshareservice.endReview(review.getId(), reviewTitle, "myDescription", "12345");
+    assertEquals(review.getId(), review.getId());
 
-        Integer completedStatus = conceptshareservice.getReviewStatusId(ConceptshareService.REVIEW_COMPLETED_STATUS);
-        assertEquals(completedStatus, review.getStatusId().getValue());
+    Integer completedStatus = conceptshareservice.getReviewStatusId(ConceptshareService.REVIEW_COMPLETED_STATUS);
+    assertEquals(completedStatus, review.getStatusId().getValue());
 
-    }
+  }
 
-    @Test
-    public void itCanRemoveItemFromReview() throws Exception {
-        long timestamp = new Date().getTime();
-        String reviewTitle = "myReviewUnitTest - " + timestamp;
-        String name = "Unit test - " + timestamp;
-        String filename = name + ".png";
-        Review review = createReview(reviewTitle);
+  @Test
+  public void itCanRemoveItemFromReview() throws Exception {
+    long timestamp = new Date().getTime();
+    String reviewTitle = "myReviewUnitTest - " + timestamp;
+    String name = "Unit test - " + timestamp;
+    String filename = name + ".png";
+    Review review = createReview(reviewTitle);
 
-        Asset asset = conceptshareservice.addAsset(name, filename, "https://www.nuxeo.com/assets/imgs/logo340x60.png");
-        ReviewItem ri = conceptshareservice.addReviewItem(review.getId(), asset.getId());
-        assertEquals(ri.getReviewId(), review.getId());
-        ri = conceptshareservice.removeReviewItem(review.getId(), ri.getAssetId());
-    }
+    Asset asset = conceptshareservice.addAsset(name, filename, "https://www.nuxeo.com/assets/imgs/logo340x60.png");
+    ReviewItem ri = conceptshareservice.addReviewItem(review.getId(), asset.getId());
+    assertEquals(ri.getReviewId(), review.getId());
+    ri = conceptshareservice.removeReviewItem(review.getId(), ri.getAssetId());
+  }
 
-    @Test
-    public void itCanVersionAsset() throws Exception{
+  @Test
+  public void itCanVersionAsset() throws Exception {
 
     long timestamp = new Date().getTime();
     String reviewTitle = "myReviewUnitTest - " + timestamp;
@@ -141,18 +141,18 @@ public class TestConceptshareService {
     newAsset = conceptshareservice.addVersionedAsset(name, newAsset.getId(), filename, assetV1);
     //conceptshareservice.removeReviewItem(review.getId(), newAsset.getId());
     //ri = conceptshareservice.addReviewItem(review.getId(), asset.getId());
-    }
+  }
 
-    public Review createReview(String reviewTitle) throws Exception {
-        Review review = conceptshareservice.createReview(reviewTitle, "myDescription", "12345");
-        assertEquals(reviewTitle, review.getTitle().getValue());
-        return review;
-    }
+  public Review createReview(String reviewTitle) throws Exception {
+    Review review = conceptshareservice.createReview(reviewTitle, "myDescription", "12345");
+    assertEquals(reviewTitle, review.getTitle().getValue());
+    return review;
+  }
 
-    @Test
-    public void itCanGetReviewStatusList() {
-        List<Status> statusList = conceptshareservice.getReviewStatusList();
-        assertTrue("Status list should not be empty", statusList.size() >= 3);
-    }
+  @Test
+  public void itCanGetReviewStatusList() {
+    List<Status> statusList = conceptshareservice.getReviewStatusList();
+    assertTrue("Status list should not be empty", statusList.size() >= 3);
+  }
 
 }
